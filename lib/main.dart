d25fad36f93bef0eb56b44118d6dcbf6a22410cb
Guilherme Ribeiro <guilherme.ribeiro@ciadoleite.com>
2022-05-07 17:55:19 -0300

    melhoria na construção dos widget e como se comunicam entre sim (refit man.dart)

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          const Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
-          ),
-          const Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Column(
-            children: const <Widget>[
-              Text(
-                'We make coding fun',
-                textDirection: TextDirection.ltr,
-              ),
-            ],
-          ),
-        ],
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Transferências'),
+        ),
+        body: ListaTransferencias(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Pressionou o botão');
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    ));
+
+class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 2000)),
+        ItemTransferencia(Transferencia(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
       ),
     );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
+}
